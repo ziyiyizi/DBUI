dcff202a7cb16931c2ee384e077213f623809e25
ziyi <xiajinlei@foxmail.com>
2018-07-18 18:45:32 +0800

    main

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult MainPage()
+        {
+            return View();
+        }
+
+        public ActionResult BookSearch()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Classify()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
